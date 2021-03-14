fcd504388a78ef654ef1fed5b49055a815384600
Anukriti Rawat <47984303+KratiAnu@users.noreply.github.com>
2021-03-14 19:25:22 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -37,7 +37,7 @@ class Home extends Component {
             {/* <Navbar history={this.props.history} /> */}
             <Navbar2 history={this.props.history} />
             <div className="container home-cont1-text">
-              <h1 className="h1 text-uppercase text-white text-center mb-4"><strong>Organic Fast Food Made <br /> Easy And Healthy</strong></h1>
+              <h1 className="h1 text-uppercase text-white text-center mb-4"><strong>Garage Components <br /> Easily Deliverable</strong></h1>
               <div className="container">
                 <div className="row justify-content-center">
                   <div className="col-lg-6 col-md-6 col-sm-12 mb-3">
@@ -61,7 +61,7 @@ class Home extends Component {
         <div className="container-fluid py-2 bg-warning">
           <div className="row">
             <div className="col-lg-4 col-md-4 col-sm-12">
-              <p className="my-3 text-lg-right text-md-right text-center text-white"><b className="mr-2 h5">18</b>Restaurant</p>
+              <p className="my-3 text-lg-right text-md-right text-center text-white"><b className="mr-2 h5">18</b>Repair Shops and Garage</p>
             </div>
             <div className="col-lg-4 col-md-4 col-sm-12">
               <p className="my-3 text-center text-white"><b className="mr-2 h5">9</b>People Served</p>
@@ -76,30 +76,30 @@ class Home extends Component {
         <div className="container-fluid text-center py-4">
           <div className="py-4">
             <h2 className="h2 text-uppercase">How It Works</h2>
-            <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+            <p>We work as per the needs of the customers, being ready to help them at any point.</p>
           </div>
           <div className="container">
             <div className="row">
               <div className="col-12 col-lg-4 col-md-4 px-5">
                 <span className="round-border my-4">
-                  <img alt="Choose A Restaurant" src={require("../assets/images/how-to-work2.png")} />
+                  <img alt="Choose A Repair Shop or Garage" src={require("../assets/images/how-to-work2.png")} />
                 </span>
-                <h3 className="h3 mb-4">Choose A Restaurant</h3>
-                <p className="mb-4">Cras vitae dictum velit. Duis at purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
+                <h3 className="h3 mb-4">Choose A Repair Shop or Garage</h3>
+                <p className="mb-4">Just Click on the Shop you want your vehicle to be fixed in, will make them available to you.</p>
               </div>
               <div className="col-12 col-lg-4 col-md-4 px-5">
                 <span className="round-border my-4">
-                  <img alt="Choose A Tasty Dish" src={require("../assets/images/how-to-work3.png")} />
+                  <img alt="Select the service/component" src={require("../assets/images/how-to-work3.png")} />
                 </span>
-                <h3 className="h3 mb-4">Choose A Tasty Dish</h3>
-                <p className="mb-4">Dictum velit. Duis at purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
+                <h3 className="h3 mb-4">Choose the right match to your need</h3>
+                <p className="mb-4">Simply click and select the right component or service you want.</p>
               </div>
               <div className="col-12 col-lg-4 col-md-4 px-5">
                 <span className="round-border my-4">
-                  <img alt="Pick Up Or Delivery" src={require("../assets/images/how-to-work1.png")} />
+                  <img alt="Pick up or Delivery or Pick up at" src={require("../assets/images/how-to-work1.png")} />
                 </span>
-                <h3 className="h3 mb-4">Pick Up Or Delivery</h3>
-                <p className="mb-4">Purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
+                <h3 className="h3 mb-4">Pick up or Delivery or Pick up at</h3>
+                <p className="mb-4">If you want a service tell us your location, will make sure to be available there ASAP, Otherwise simply select the component/part you need.</p>
               </div>
             </div>
           </div>
@@ -107,16 +107,16 @@ class Home extends Component {
 
         {/* Home Order now section */}
         <div className="container-fluid text-center py-5 home-cont3">
-          <p className="h1 text-uppercase text-white mt-5 mb-3">Just Order And We Will Deliver You</p>
-          <p className="text-white mb-3">Pellentesque eget justo eget nibh luctus semper at ut tellus.</p>
+          <p className="h1 text-uppercase text-white mt-5 mb-3">Just Tell Us The Location We Will Reach You/Just Order And We Will Deliver You</p>
+          <p className="text-white mb-3">Choose the destination you want the service at.</p>
           <button type="button" className="btn btn-warning text-uppercase mb-5" onClick={() => this.handleOrderNowBtn()}><b>Order Now</b></button>
         </div>
 
         {/* Home Featured restaurant section */}
         <div className="container-fluid py-5">
           <div className="py-4">
-            <h2 className="h2 text-uppercase text-center">Featured Restaurant</h2>
-            <p className="text-center">Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+            <h2 className="h2 text-uppercase text-center">Featured Repair Shops and Garages</h2>
+            <p className="text-center">The list of the registered repair shops and garages with us.</p>
           </div>
           <div className="container">
             <div className="row">
@@ -124,11 +124,11 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Natural Healthy Food" src={require("../assets/images/listing-logo03.png")} />
+                      <img style={{ width: "70%" }} alt="Important Parts And Services" src={require("../assets/images/listing-logo03.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
-                      <h5 className="mb-1">Natural Healthy Food</h5>
-                      <p className="mb-2"><small>Apple Juice, Beef Roast, Cheese Burger</small></p>
+                      <h5 className="mb-1">Important Parts And Services</h5>
+                      <p className="mb-2"><small>Oil Servicing, Tire Change, Battery Check, Cluthch Operation Check, Coolant Check and change, Belts and Body check</small></p>
                       <p>
                         <small className="">
                           <FontAwesomeIcon icon="star" className="rating mr-1" />
@@ -148,11 +148,11 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Menu & Drinks" src={require("../assets/images/listing-logo09.png")} />
+                      <img style={{ width: "70%" }} alt="Other Services" src={require("../assets/images/listing-logo09.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
                       <h5 className="mb-1">Menu &amp; Drinks</h5>
-                      <p className="mb-2"><small>Chicken Roast, Chines Soup, Cold Coffee</small></p>
+                      <p className="mb-2"><small>Body and door check, Power Steering Oil Top, Spark Pluge Clean, Engine Oil Change</small></p>
                       <p>
                         <small className="">
                           <FontAwesomeIcon icon="star" className="rating mr-1" />
@@ -172,7 +172,7 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Chefs" src={require("../assets/images/listing-logo12.png")} />
+                      <img style={{ width: "70%" }} alt="Mechanics and Workforce" src={require("../assets/images/listing-logo12.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
                       <h5 className="mb-1">Chefs</h5>
@@ -196,11 +196,11 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Menu's" src={require("../assets/images/listing-logo15.png")} />
+                      <img style={{ width: "70%" }} alt="List of parts" src={require("../assets/images/listing-logo15.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
-                      <h5 className="mb-1">Menu's</h5>
-                      <p className="mb-2"><small>Fish Fry, Fresh Juice, Stakes</small></p>
+                      <h5 className="mb-1">List of Parts</h5>
+                      <p className="mb-2"><small>Engine, Tires, Door, Gear Oil, Power Window Operation, Body Parts</small></p>
                       <p>
                         <small className="">
                           <FontAwesomeIcon icon="star" className="rating mr-1" />
@@ -220,11 +220,11 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Food N&H" src={require("../assets/images/2.png")} />
+                      <img style={{ width: "70%" }} alt="Servicing" src={require("../assets/images/2.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
-                      <h5 className="mb-1">Food N&amp;H</h5>
-                      <p className="mb-2"><small>Beef Roast, Cheese Burger, Doughnut</small></p>
+                      <h5 className="mb-1">Servicing</h5>
+                      <p className="mb-2"><small>Washing and Cleaning, Light Check, Wiper Chcek, Disk Clean Check</small></p>
                       <p>
                         <small className="">
                           <FontAwesomeIcon icon="star" className="rating mr-1" />
@@ -244,11 +244,11 @@ class Home extends Component {
                 <div className="container res-shadow res-border">
                   <div className="row p-3">
                     <div className="col-lg-4 col-md-4 col-sm-12 text-center border p-2">
-                      <img style={{ width: "70%" }} alt="Restaurant" src={require("../assets/images/listing-logo13.png")} />
+                      <img style={{ width: "70%" }} alt="Repair Shops and Garages" src={require("../assets/images/listing-logo13.png")} />
                     </div>
                     <div style={{ position: "relative" }} className="col-lg-8 col-md-8 col-sm-12 py-2">
-                      <h5 className="mb-1">Restaurant</h5>
-                      <p className="mb-2"><small>Apple Juice, BB.Q</small></p>
+                      <h5 className="mb-1">Repair Shops and Garages</h5>
+                      <p className="mb-2"><small>Clutch Operation Check, Vacuum Cleaning.Q</small></p>
                       <p>
                         <small className="">
                           <FontAwesomeIcon icon="star" className="rating mr-1" />
